d0875836d3566526aff779aa31f7948e35c4905f
chiounan <chiounan@gmail.com>
2023-09-18 22:26:27 +0800

    instance variable and constructor

---
+++ InstanceVariable.java
@@ -0,0 +1,22 @@
+public class InstanceVariable {
+    // Instance variable
+    String geek;
+    int i;
+    Integer I;
+    // default Constructor
+    InstanceVariable() {
+        this.geek = "initialize the instance variable";
+    }
+    public static void main(String args[]) {
+        // Create object
+        InstanceVariable iv = new InstanceVariable();
+        // set values for object
+        iv.geek = "Test";
+        iv.i = 1;
+        iv.I = 2;
+
+        System.out.println("InstanceVariable String: " + iv.geek);
+        System.out.println("InstanceVariable int: " + iv.i);
+        System.out.println("InstanceVariable Integer: " + iv.I);
+    }
+}
