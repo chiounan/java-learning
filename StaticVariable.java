95d345e653da3b09589f5eb382bdaa0faca6413f
cnchen.pu <cnchen@pu.edu.tw>
2023-09-19 14:36:38 +0800

    access static variable without object creation

--- StaticVariable.java
+++ StaticVariable.java
@@ -8,8 +8,6 @@ public class StaticVariable {
     public static void main (String args[]) {
         System.out.println("geek: " + StaticVariable.geek);
         // Static variable can be accessed without object creation (new)
-        
-        StaticVariable sv = new StaticVariable();
-        System.out.println("geek: " + sv.geek);
+        // StaticVariable sv = new StaticVariable();
     }
 }
