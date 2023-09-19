53e0f8e604ab5e1e4439c47fef436197c47f5e76
cnchen.pu <cnchen@pu.edu.tw>
2023-09-19 14:10:41 +0800

    accessing static variable

--- Variables.java
+++ Variables.java
@@ -1,6 +1,7 @@
 public class Variables {
     // Static variable
     static String sta1 = "sta1";
+    static String sta2 = "sta2";
     
     // Declar instance variable outside method or any block
     String ins1;
@@ -20,7 +21,8 @@ public class Variables {
         // Local variable
         String lo1 = "lo1";
 
-        System.out.println("Static variable: " + sta1);
+        System.out.println("Static variable 1: " + sta1);
+        System.out.println("Static variable 2: " + Variables.sta2);
         System.out.println("Instance variable: " + va.ins1);
         System.out.println("Local variable: " + lo1);
     }
