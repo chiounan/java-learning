c3e27056de5ab3fdaebf8aab189342b2fe988c83
cnchen.pu <cnchen@pu.edu.tw>
2023-10-06 17:48:23 +0800

    update

--- ArraysClass.java
+++ ArraysClass.java
@@ -1,3 +1,10 @@
+// Arrays class:
+// Arrays.toString
+// Arrays.binarySearch
+// Arrays.copyOfRange
+// Arrays.equals
+// Arrays.fill
+
 import java.util.Arrays;
 
 public class ArraysClass {
@@ -9,7 +16,7 @@ public class ArraysClass {
     }
 
     public static void main(String args[]) {
-        int arr1[] = {10, 20, 15, 22, 35};
+        int[] arr1 = {10, 20, 15, 22, 35};
 
         System.out.print("Array1: " + Arrays.toString(arr1));
         //Arritem(arr1);
@@ -30,6 +37,19 @@ public class ArraysClass {
 
         //Copy part of array to a string
         System.out.println("Array range between index 1 and 3: " + Arrays.toString(Arrays.copyOfRange(arr1, 1, 3)));
+        
+        // Compare two arrays
+        int[] arr2 = {10, 20, 15, 22, 35};
+        System.out.println("arr1: " + Arrays.toString(arr1));
+        System.out.println("arr2: " + Arrays.toString(arr2));
+        System.out.println("Arrays comparison: " + Arrays.equals(arr1, arr2));
+
+        // To fill the array
+        int[] arr3;     // Using "int arr3[]" is fine.
+        arr3 = new int[5];      // Better usage is: int[] arr3 = new int [5];
+        int arrkey = 22;
+        Arrays.fill(arr3, arrkey);
+        System.out.println("Fill the array with " + arrkey + ": " + Arrays.toString(arr3));
 
     }
 }
